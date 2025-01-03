bc217ca54f4aeb57912fa985a4063dfd9a9d4005
jondow72 <77569928+jondow72@users.noreply.github.com>
2025-01-03 16:28:10 +0100

    Fix warnings placeholders
    /usr/include/boost/bind.hpp: At global scope:
    /usr/include/boost/bind.hpp:36:1: note: ‘#pragma message: The practice of declaring the Bind placeholders (_1, _2, ...) in the global namespace is deprecated. Please use <boost/bind/bind.hpp> + using namespace boost::placeholders, or define BOOST_BIND_GLOBAL_PLACEHOLDERS to retain the current behavior.’
       36 | BOOST_PRAGMA_MESSAGE(
          | ^~~~~~~~~~~~~~~~~~~~

--- src/qt/clientmodel.cpp
+++ src/qt/clientmodel.cpp
@@ -8,8 +8,13 @@
 #include "main.h"
 #include "ui_interface.h"
 
+#include <boost/bind/bind.hpp>
+#include <boost/bind/placeholders.hpp>
+
 #include <QDateTime>
 #include <QTimer>
+
+using namespace boost::placeholders;
 
 static const int64 nClientStartupTime = GetTime();
 
